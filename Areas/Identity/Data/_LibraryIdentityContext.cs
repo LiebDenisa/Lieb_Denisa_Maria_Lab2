78b0665ec969396d0626f28871d67dfbb4c1bd2b
LiebDenisa <denisalieb@gmail.com>
2024-11-05 12:33:03 +0200

    Laborator 5 + sarcina lab

---
+++ Areas/Identity/Data/_LibraryIdentityContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lieb_Denisa_Maria_Lab2.Data;
+
+public class _LibraryIdentityContext : IdentityDbContext<IdentityUser>
+{
+    public _LibraryIdentityContext(DbContextOptions<_LibraryIdentityContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
